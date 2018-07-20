947d9e2847f0421e5dcd34d3557a6bf7a881a333
alex <a.loretzin@gmail.com>
2018-07-20 13:48:05 +0200

    ale

---
+++ js/anniversaire.js
@@ -0,0 +1,22 @@
+
+let clickBoite = $('.clickBoite')
+
+$("#clickBoite1").on("click", function () {
+    $(this).fadeOut()
+    $("#clickBoite2").fadeIn()
+    $('#clickBoite2').click(function () {
+        $(this).fadeOut()
+        $('#clickBoite3').fadeIn()
+        $('#clickBoite3').click(function () {
+            $(this).fadeOut()
+            $('.clickBoite').fadeOut()
+            $('.content').css({
+                "background-image": "url('https://www.123cartes.com/wp-content/uploads/2016/05/carte-joyeux-anniversaire-confettis.jpg')",
+                'backgroundRepeat': 'no-repeat',
+                'backgroundSize': '100%'
+            })
+
+        })
+    })
+});
+
