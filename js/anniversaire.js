7b244787a0d02514702f4d7b30bf63e08db81226
alex <a.loretzin@gmail.com>
2018-07-20 13:55:52 +0200

    ale

--- js/anniversaire.js
+++ js/anniversaire.js
@@ -4,12 +4,12 @@ let clickBoite = $('.clickBoite')
 $("#clickBoite1").on("click", function () {
     $(this).fadeOut()
     $("#clickBoite2").fadeIn()
-    $('#clickBoite2').click(function () {
+    $('#clickBoite2').on("click", function () {
         $(this).fadeOut()
         $('#clickBoite3').fadeIn()
-        $('#clickBoite3').click(function () {
+        $('#clickBoite3').on("click", function () {
             $(this).fadeOut()
-            $('.clickBoite').fadeOut()
+            $('.clickBoite').fadeIn()
             $('.content').css({
                 "background-image": "url('https://www.123cartes.com/wp-content/uploads/2016/05/carte-joyeux-anniversaire-confettis.jpg')",
                 'backgroundRepeat': 'no-repeat',
