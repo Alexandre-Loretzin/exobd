55cbae057b14be1da9e5c09bec81f6289ce24aa6
alex <a.loretzin@gmail.com>
2018-07-20 14:29:02 +0200

    optimisation

--- js/anniversaire.js
+++ js/anniversaire.js
@@ -1,9 +1,9 @@
 $("#clickBoite1").on("click", function () {
-    $(this).fadeOut()
+    $(this).animate({opacity : 0})
     $("#clickBoite2").fadeIn().on("click", function () {
-        $(this).fadeOut()
+        $(this).animate({opacity : 0})
         $('#clickBoite3').fadeIn().on("click", function () {
-            $(this).fadeOut()
+            $(this).animate({opacity : 0})
             $('.content').addClass("background")
         })
     })
